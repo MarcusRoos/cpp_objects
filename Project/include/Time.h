48251019714f85b3445bc7208f8b5c6531483d20
Marcus Roos <Marcusrs_roos@outlook.com>
2020-03-18 06:18:33 +0100

    Author: Marcus Roos Last updated: 18/03-2020 Task: Project
    Progress: Updated classes.

--- Project/include/Time.h
+++ Project/include/Time.h
@@ -1,27 +1,24 @@
-//
-// Created by Marcus Roos on 2020-03-16.
-// Mittuniversitet
-// StudentID: Maro1904
-//
 
 #ifndef DT019G_TIME_H
 #define DT019G_TIME_H
+
 class Time {
 private:
     int hour, min, sec;
+
 public:
+
     Time();
-    Time(int pHour=0, int pMin=0, int pSec=0);
+    Time(int pHour, int pMin=0, int pSec=0);
 
     void setHour(int pHour);
     void setMin(int pMin);
-    void setSec(int pSec);
+    void setSec(const int pSec);
 
-    int getHour()const {return hour;}
-    int getMin()const {return min;}
-    int getSec()const {return sec;}
-
-    void tic();
+    int getHour() const { return hour; }
+    int getMin() const { return min; }
+    int getMin() const { return sec; }
 
 };
-#endif //DT019G_TIME_H
+
+#endif
--- Project/src/Time.cpp
+++ Project/src/Time.cpp
@@ -1,10 +1,9 @@
-//
-// Created by Marcus Roos on 2020-03-16.
-// Mittuniversitet
-// StudentID: Maro1904
-//
 
+#include <iostream>
+#include <iomanip>    // setw, setfill
 #include "Time.h"
+using namespace std;
+
 
 Time::Time() {
     hour = min = sec = 0;
@@ -16,24 +15,13 @@ Time::Time(int pHour, int pMin, int pSec) {
     setSec(pSec);
 }
 
-void Time::setHour(int pHour) {
-    if(pHour >= 0 && pHour < 24) hour = pHour;
-    else
-        hour = -1;
+void Time::setHour(const int pHour) {
+    hour = (pHour >= 0 && pHour < 24) ? pHour : -1;
 }
 
-void Time::setMin(int pMin) {
-    if(pMin >= 0 && pMin < 60) min = pMin;
-    else
-        min = -1;
+void Time::setMin(const int pMin) {
+    min = (pMin >= 0 && pMin < 60) ? pMin : -1;
 }
-
-void Time::setSec(int pSec) {
-    if(pSec >= 0 && pSec < 60) sec = pSec;
-    else
-        sec = -1;
-}
-
-void Time::tic() {
-
+void Time::setSec(const int pSec) {
+    sec = (pSec >= 0 && pSec < 60) ? pSec : -1;
 }
